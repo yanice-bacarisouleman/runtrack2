68c041560c8037935e8157dcff8b0e1900d931dc
yanice-bacarisouleman <yanice.bacari-souleman@laplateforme.io>
2021-11-09 10:36:05 +0100

    Tout les jobs journée 5 rajouté vide

---
+++ jour04/job06/index.php
@@ -0,0 +1,36 @@
+
+<?php
+$str = "On n est pas le meilleur quand
+on le croit mais quand on le sait";
+
+$dic= ["voyelles" => ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'],
+"consonnes" => ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z', 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Z']];
+
+foreach ($dic as $ $voyelles) {
+        
+}
+
+?>
+
+
+
+
+
+<html> 
+
+<table>
+<tr>
+    <thead>
+        <th>Voyelles</th>
+        <th>Consonnes</th> 
+</tr>    
+    </thead>
+    <tbody>
+<tr>
+
+
+
+</tr>
+    </tbody>
+</table>
+    
